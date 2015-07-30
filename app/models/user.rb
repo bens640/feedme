3bfbb233a71097879be60c5713b6a5c07158dc0d
Weston Cooper <weston@westoncooper.com>
2015-07-29 20:58:17 -0400

    merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reservations
   validates :first_name, presence: true
-<<<<<<< HEAD
-  # validates :last_name, presence: true
-=======
   validates :last_name, presence: true
->>>>>>> 446b09131a913efa949365fde900ab804e268b66
   validates :email, uniqueness: true, confirmation: true
   # validates :email_confirmation, presence: true
   validates :password_digest, confirmation: true
-<<<<<<< HEAD
-  # validates :password_digest_confirmation, presence: true
-  has_secure_password
-=======
   validates :password_digest_confirmation, presence: true
 
 
@@ -21,5 +13,4 @@ def index
 
 end
 
->>>>>>> 446b09131a913efa949365fde900ab804e268b66
 end
