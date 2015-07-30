446b09131a913efa949365fde900ab804e268b66
Sean H <seathony@gmail.com>
2015-07-29 20:11:20 -0400

    going through errors login

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,7 +3,6 @@ class Chef < ActiveRecord::Base
   has_many :reservations
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :nickname, presence: true, uniqueness: true
   validates :email, uniqueness: true, confirmation: true
   validates :email_confirmation, presence: true
   validates :password_digest, confirmation: true
