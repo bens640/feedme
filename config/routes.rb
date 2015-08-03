278561eb12c4d242bbdc1782421f135bc96d2f0d
Ben Shelomovitz <bens640@gmail.com>
2015-08-03 15:04:01 -0400

    Add more styling and fixed reservation bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get 'messages/new'
 
+
+  get 'reservations/new'
   get 'login_chef' => 'sessions#new_chef'
   get 'login_user' => 'sessions#new_user'
 
@@ -20,14 +22,14 @@ Rails.application.routes.draw do
   resources :reservations do
     resources :messages
   end
-
-  post 'reservations/show'
+  get 'reservations/show'
+  patch 'reservations/show'
 
   root 'users#index'
 
   get '/logout', to: 'sessions#destroy'
 
-  get 'reservations/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
