19cad6d886b83ee00660271cf4ee0b4c95642895
westongcooper <cooperweston@gmail.com>
2015-08-05 12:40:01 -0400

    stripe in test mode

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+    :publishable_key => Rails.application.secrets.stripe_publishable_key,
+    :secret_key      => Rails.application.secrets.stripe_secret_key
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
-StripeEvent.authentication_secret = ENV['STRIPE_WEBHOOK_SECRET']
-StripeEvent.authentication_secret = Rails.application.secrets.stripe_webhook_secret
+Stripe.api_key = Rails.application.secrets.stripe_secret_key
