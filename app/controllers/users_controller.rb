9d9af6615d12b0474b75c5d72a965afc384cccc6
Ben Shelomovitz <bens640@gmail.com>
2015-08-06 18:57:28 -0400

    Removed Emailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       session[:user_id] = @user.id
-      UserMailer.welcome_email(@user.email).deliver
+      # UserMailer.welcome_email(@user.email).deliver
       redirect_to @user , flash:{notice: 'created user and logged in'}
     else
       render action: 'new', flash:{notice:'please try again'}
