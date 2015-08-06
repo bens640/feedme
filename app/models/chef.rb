839f9a8ab5af0ad803015bab296b27c526f55d5a
Ben Shelomovitz <bens640@gmail.com>
2015-08-06 12:38:16 -0400

    Add new reservation link to recipe images on home page

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,8 +2,7 @@ class Chef < ActiveRecord::Base
   has_secure_password
   has_many :reservations
   has_many :messages
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, :last_name, :address, :city, :zip, :state, :phone, presence: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :email, uniqueness: true
   validates_format_of :phone, :with => /\A(1?(?: |\-|\.)?(?:\(\d{3}\)|\d{3})(?: |\-|\.)?\d{3}(?: |\-|\.)?\d{4})\z/, message: "bad format ex. 555-555-5555"
